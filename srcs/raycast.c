687de58845b33737e579a08ec3e6a7c87f8d8239
Ana <anaisabelfp2003@gmail.com>
2026-05-15 03:05:30 +0100

    suggest norminette

--- srcs/raycast.c
+++ srcs/raycast.c
@@ -41,6 +41,101 @@ static void	calc_step(t_player *player, t_ray *ray)
 	else
 	{
 		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - player->pos_x)
+			* ray->delta_dist_x;
+	}
+	if (ray->ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (player->pos_y - ray->map_y) * ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - player->pos_y)
+			* ray->delta_dist_y;
+	}
+}
+
+static void	perform_dda(t_map *map, t_ray *ray)
+{
+	ray->hit = 0;
+	while (ray->hit == 0)
+	{
+		if (ray->side_dist_x < ray->side_dist_y)
+		{
+			ray->side_dist_x += ray->delta_dist_x;
+			ray->map_x += ray->step_x;
+			ray->side = 0;
+		}
+		else
+		{
+			ray->side_dist_y += ray->delta_dist_y;
+			ray->map_y += ray->step_y;
+			ray->side = 1;
+		}
+		if (ray->map_x < 0 || ray->map_y < 0 || ray->map_x >= map->width
+			|| ray->map_y >= map->height)
+			ray->hit = 1;
+		else if (map->matrix[ray->map_y][ray->map_x] == '1')
+			ray->hit = 1;
+	}
+}
+
+void	cast_ray(t_player *player, t_map *map, t_ray *ray, int x)
+{
+	init_ray(player, ray, x);
+	calc_step(player, ray);
+	perform_dda(map, ray);
+	if (ray->side == 0)
+		ray->perp_wall_dist = (ray->side_dist_x - ray->delta_dist_x);
+	else
+		ray->perp_wall_dist = (ray->side_dist_y - ray->delta_dist_y);
+}
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycast.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/06 03:43:33 by adias-do          #+#    #+#             */
+/*   Updated: 2026/05/09 14:22:24 by adias-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+#include "../include/cub3d.h"
+
+static void	init_ray(t_player *player, t_ray *ray, int x)
+{
+	double	camera_x;
+
+	camera_x = 2.0 * x / (double)WIN_WIDTH - 1.0;
+	ray->ray_dir_x = player->dir_x + player->plane_x * camera_x;
+	ray->ray_dir_y = player->dir_y + player->plane_y * camera_x;
+	ray->map_x = (int)player->pos_x;
+	ray->map_y = (int)player->pos_y;
+	if (ray->ray_dir_x == 0)
+		ray->delta_dist_x = 1e30;
+	else
+		ray->delta_dist_x = fabs(1.0 / ray->ray_dir_x);
+	if (ray->ray_dir_y == 0)
+		ray->delta_dist_y = 1e30;
+	else
+		ray->delta_dist_y = fabs(1.0 / ray->ray_dir_y);
+}
+
+static void	calc_step(t_player *player, t_ray *ray)
+{
+	if (ray->ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (player->pos_x - ray->map_x) * ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
 		ray->side_dist_x = (ray->map_x + 1.0 - player->pos_x) * ray->delta_dist_x;
 	}
 	if (ray->ray_dir_y < 0)
@@ -89,3 +184,4 @@ void	cast_ray(t_player *player, t_map *map, t_ray *ray, int x)
 	else
 		ray->perp_wall_dist = (ray->side_dist_y - ray->delta_dist_y);
 }
+*/
--- srcs/textures.c
+++ srcs/textures.c
@@ -12,6 +12,108 @@
 
 #include "../include/cub3d.h"
 
+static void	verify_texture(void *mlx, t_texture *tex, char *path)
+{
+	if (tex->width <= 0 || tex->height <= 0)
+	{
+		printf("Error\nInvalid dimensions: %s (%dx%d)\n",
+			path, tex->width, tex->height);
+		mlx_destroy_image(mlx, tex->img);
+		exit(1);
+	}
+	tex->addr = mlx_get_data_addr(tex->img,
+			&tex->bpp, &tex->line_len, &tex->endian);
+	if (!tex->addr)
+	{
+		printf("Error\nFailed to get data address: %s\n", path);
+		mlx_destroy_image(mlx, tex->img);
+		exit(1);
+	}
+	if (tex->bpp <= 0 || tex->line_len <= 0)
+	{
+		printf("Error\nInvalid data format: %s\n", path);
+		mlx_destroy_image(mlx, tex->img);
+		exit(1);
+	}
+}
+
+void	load_texture(void *mlx, t_texture *tex, char *path)
+{
+	if (!mlx || !tex || !path || !*path)
+	{
+		printf("Error\nInvalid or empty texture path\n");
+		exit(1);
+	}
+	tex->img = mlx_xpm_file_to_image(mlx, path,
+			&tex->width, &tex->height);
+	if (!tex->img)
+	{
+		printf("Error\nTexture load failed: %s\n", path);
+		exit(1);
+	}
+	verify_texture(mlx, tex, path);
+}
+
+t_texture	*get_wall_texture(t_game *game, t_ray *ray)
+{
+	if (ray->side == 0)
+	{
+		if (ray->ray_dir_x > 0)
+			return (&game->east);
+		else
+			return (&game->west);
+	}
+	else
+	{
+		if (ray->ray_dir_y > 0)
+			return (&game->south);
+		else
+			return (&game->north);
+	}
+}
+
+int	get_tex_color(t_texture *tex, int x, int y)
+{
+	char	*dst;
+
+	if (x < 0 || x >= tex->width || y < 0 || y >= tex->height)
+		return (0);
+	dst = tex->addr + (y * tex->line_len + x * (tex->bpp / 8));
+	return (*(unsigned int *)dst);
+}
+
+int	calc_tex_x(t_game *game, t_ray *ray, t_texture *current_tex)
+{
+	double	wall_x;
+	int		tex_x;
+
+	if (ray->side == 0)
+		wall_x = game->player.pos_y + ray->perp_wall_dist * ray->ray_dir_y;
+	else
+		wall_x = game->player.pos_x + ray->perp_wall_dist * ray->ray_dir_x;
+	wall_x -= floor(wall_x);
+	tex_x = (int)(wall_x * (double)current_tex->width);
+	if (ray->side == 0 && ray->ray_dir_x < 0)
+		tex_x = current_tex->width - tex_x - 1;
+	if (ray->side == 1 && ray->ray_dir_y > 0)
+		tex_x = current_tex->width - tex_x - 1;
+	return (tex_x);
+}
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   textures.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adias-do <adias-do@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/12 03:43:56 by adias-do          #+#    #+#             */
+/*   Updated: 2026/05/09 14:22:47 by adias-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*
+#include "../include/cub3d.h"
+
 void	load_texture(void *mlx, t_texture *tex, char *path)
 {
 	if (!mlx || !tex || !path)
@@ -105,3 +207,4 @@ int	calc_tex_x(t_game *game, t_ray *ray, t_texture *current_tex)
 		
 	return (tex_x);
 }
+*/
